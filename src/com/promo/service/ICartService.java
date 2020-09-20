01c2d9fd35ef70fab6d2d158d603dee90b22db7b
Sachin D Gadade <sachinorld@gmail.com>
2020-09-20 22:01:29 +0530

    Added comments on methods

--- src/com/promo/service/ICartService.java
+++ src/com/promo/service/ICartService.java
@@ -6,6 +6,24 @@ import com.promo.model.Promotion;
 
 public interface ICartService extends BaseService {
 
+	/**
+	 * Add items to cart. item becomes cartItem
+	 * 
+	 * @param cart
+	 * @param item
+	 * @param quantity
+	 * @return
+	 * @throws Exception
+	 */
 	public boolean addToCart(Cart cart, Item item, int quantity) throws Exception;
+	/**
+	 * apply promotion on the cart. 
+	 * reduces the overall cart price based on promotion
+	 * 
+	 * @param cart
+	 * @param promotion
+	 * @return
+	 * @throws Exception
+	 */
 	public Cart executePromotions(Cart cart, Promotion promotion) throws Exception;
 }
