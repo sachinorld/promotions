a08dd021d0984df2b03f939d20175aa79c654ba4
Sachin D Gadade <sachinorld@gmail.com>
2020-09-20 19:55:51 +0530

    Added method implementation
    Added method implementation and utility methods

--- src/com/promo/service/CartService.java
+++ src/com/promo/service/CartService.java
@@ -1,5 +1,9 @@
 package com.promo.service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.promo.model.Cart;
 import com.promo.model.CartItem;
 import com.promo.model.Item;
@@ -8,19 +12,77 @@ import com.promo.model.Promotion;
 public class CartService implements ICartService {
 
 	@Override
-	public boolean addToCart(Cart cart, Item item, int quantity) {
-		return false;
+	public boolean addToCart(Cart cart, Item item, int quantity) throws Exception {
+		CartItem cartItem;
+		try {
+			System.out.println("CartService.addToCart - " + item.getSku());
+			item.setQuantity(quantity);
+			cartItem = new CartItem(item);
+		} catch (Exception e) {
+			throw new Exception("Unable to add to cart. Please verify the Item");
+		}
+		if (cart.getCartItems().containsKey(item.getSku())) {
+			cartItem = cart.getCartItems().get(item.getSku());
+			cartItem.setQuantity(cartItem.getQuantity() + item.getQuantity());
+			cartItem.setCartPrice(cartItem.getQuantity() * item.getPrice());
+		}
+		cart.getCartItems().put(item.getSku(), cartItem);
+		return SUCCESS;
 	}
 
 	@Override
-	public Cart executePromotions(Cart cart, Promotion promotion) {
-		return new Cart();
+	public Cart executePromotions(Cart cart, Promotion promotion) throws Exception {
+		System.out.println("CartService.executePromotions - " + promotion.getPromoCode());
+		Map<String, CartItem> cartItems = cart.getCartItems();
+
+		List<String> skuList = promotion.getItems().stream().map(Item::getSku).collect(Collectors.toList());
+		boolean cartContains = cartItems.keySet().containsAll(skuList);
+		
+		if (promotion.getItems().size() < 1 || !cartContains) {
+			return cart;
+		}
+		if (!promotion.isCombinedPromotion()) {
+			// ex: cart has 5'A each for 20Rs
+			//     promo has 2'A for 30Rs
+			//     total price = 5*20=100Rs. Promo price = 60+20=80Rs
+			Item promoItem = promotion.getItems().get(0);
+			CartItem cartItem = cartItems.get(promoItem.getSku());
+			if (!promoApplied(cartItem)) {
+				updateCartItem(promotion, cartItem, promoItem);
+			}
+		} else {
+			// ex: cart has 1'C=25Rs and 1'D=30Rs
+			//     promo has 1'C+1'D=45Rs
+			//     total price=55Rs. promoprice - 1'C=0, 1'D=45Rs
+			int size = promotion.getItems().size(); // 2
+			for (int i = 0; i < size; i++) {
+				CartItem cartItem = cartItems.get(promotion.getItems().get(i).getSku());
+				if (promoApplied(cartItem))
+					break;
+				
+				cartItem.setCartPrice(0); // C.price=0
+				if (i == size - 1) { // i=1
+					cartItem.setCartPrice(promotion.getPromoPrice()); // D.price=45
+				}
+				cartItem.setPromoApplied(true);
+			}
+		}
+		return cart;
 	}
 
 	private boolean promoApplied(CartItem cartItem) {
-		return false;
+		return cartItem.isPromoApplied();
 	}
 
-	
+	private void updateCartItem(Promotion promotion, CartItem cartItem, Item promoItem) {
+		int promoQuantity = promoItem.getQuantity();//2
+		int presentQuantity = cartItem.getQuantity();//5
+		int applyTimes = presentQuantity / promoQuantity;// 5/2=2
+		int remainingQuantity = presentQuantity - applyTimes * promoQuantity;// 5-(2*2)=1
+		
+		double regularPrice = cartItem.getPrice();// 20
+		cartItem.setCartPrice((remainingQuantity * regularPrice) + (applyTimes * promotion.getPromoPrice()));
+		cartItem.setPromoApplied(true);
+	}
 
 }
